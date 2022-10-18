248572b72cebd7048893958c39fc9593789462b6
Jonni Åkesson <enterthesource@me.com>
2022-10-18 16:47:53 +0200

    Initial Commit

---
+++ CustomGrid/CustomGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomGridApp.swift
+//  CustomGrid
+//
+//  Created by Jonni Akesson on 2022-10-18.
+//
+
+import SwiftUI
+
+@main
+struct CustomGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
